83e788f40adcf90122f9dc5d0512a93a47477359
Stanislav-Netreba <stasnetreba1322@gmail.com>
2023-06-17 14:41:50 +0200

    added styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,20 @@
-import '@/styles/publishProject.css'
-import { StatusContext } from '@/context/context'
-import { useState } from 'react'
+import '@/styles/PublishProject.css';
+import { StatusContext } from '@/context/context';
+import { useState } from 'react';
 // import Nav from '@/components/Nav/Nav'
 // import Footer from '@/components/Footer/Footer'
-import{ Analytics } from'@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  const [status,setStatus] = useState(false)
-  return (
-  <StatusContext.Provider value={{status,setStatus}}>
-      {/* <header>        
+    const [status, setStatus] = useState(false);
+    return (
+        <StatusContext.Provider value={{ status, setStatus }}>
+            {/* <header>        
           <Nav/>
         </header> */}
-    <Component {...pageProps} />
-    {/* <Footer/> */}
-    <Analytics/>
-  </StatusContext.Provider>
-  )
+            <Component {...pageProps} />
+            {/* <Footer/> */}
+            <Analytics />
+        </StatusContext.Provider>
+    );
 }
